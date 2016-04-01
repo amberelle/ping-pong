d7232f51589f17726d96d9ab01c83e7b887992c3
Amber Baird <amber.baird@gmail.com>
2016-04-01 15:14:16 -0700

    add css stylings and gets rid of the clear button

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,9 @@
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
     event.preventDefault();
+    $("#list").text("");
     var score = parseInt($("#number_input").val());
-    var result = pingandpong(score);
-    $("#result").text(result);
+    pingandpong(score);
   });
 });
 
@@ -12,15 +12,17 @@ $(document).ready(function() {
 
 //business logic
 var pingandpong = function(score) {
-for (var index = 1; index <= score; index +=1) {
-  if (index % 3 === 0 && index % 5 === 0) {
-      $("#list").append("<li>" + "pingpong" + "</li>");
-  } else if (index % 5 === 0){
-      $("#list").append("<li>" + "pong" + "</li>");
-  } else if (index % 3 === 0){
-      $("#list").append("<li>" + "ping" + "</li>");
-  } else {
+
+  for (var index = 1; index <= score; index +=1) {
+
+    if (index % 3 === 0 && index % 5 === 0) {
+      $("#list").append("<li>pingpong</li>");
+    } else if (index % 5 === 0){
+      $("#list").append("<li>pong</li>");
+    } else if (index % 3 === 0){
+      $("#list").append("<li>ping</li>");
+    } else {
       $("#list").append("<li>" + index + "</li>");
-  }
-};
+    }
+  };
 };
