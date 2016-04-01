97b5a83219e4fb667d897595137c3aae2da7a74d
Amber Baird <amber.baird@gmail.com>
2016-04-01 11:20:47 -0700

    first working website commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,3 @@
-//business logic
-var pingandpong = function(score) {
-for (var index = 1; index <= score; index +=1) {
-  if (index % 3 === 0 && index % 5 === 0) {
-      $("#list").append("<li>" + "ping" + "</li>");
-  } else if (index % 5 === 0){
-      $("#list").append("<li>" + "ping" + "</li>");
-  } else if (index % 3 === 0){
-      $("#list").append("<li>" + "ping" + "</li>");
-  } else {
-      $("#list").prompt("please enter a number");
-  }
-};
-
-
-
-
-
-
-
-
-
-
-
-
 //user interface logic
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
@@ -32,3 +7,20 @@ $(document).ready(function() {
     $("#result").text(result);
   });
 });
+
+
+
+//business logic
+var pingandpong = function(score) {
+for (var index = 1; index <= score; index +=1) {
+  if (index % 3 === 0 && index % 5 === 0) {
+      $("#list").append("<li>" + "pingpong" + "</li>");
+  } else if (index % 5 === 0){
+      $("#list").append("<li>" + "pong" + "</li>");
+  } else if (index % 3 === 0){
+      $("#list").append("<li>" + "ping" + "</li>");
+  } else {
+      $("#list").append("<li>" + index + "</li>");
+  }
+};
+};
