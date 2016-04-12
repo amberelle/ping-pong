0d4adc9ce80f28ad3023e8bdade3de67c4d6f94f
Amber Baird <amber.baird@gmail.com>
2016-04-12 09:57:36 -0700

    seperated user interface and business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
+
+
+
+//business logic
+var pingandpong = function(score) {
+  var numbers = [];
+
+  for (var index = 1; index <= score; index +=1) {
+
+    if (index % 3 === 0 && index % 5 === 0) {
+      numbers[index] = "pingpong"
+    } else if (index % 5 === 0){
+      numbers[index] = "pong"
+    } else if (index % 3 === 0){
+      numbers[index] = "ping"
+    } else {
+      numbers[index] = index
+    }
+    $("#list").append("<li>" + numbers[index] + "</li>");
+  };
+};
+
+
 //user interface logic
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
@@ -7,22 +30,3 @@ $(document).ready(function() {
     pingandpong(score);
   });
 });
-
-
-
-//business logic
-var pingandpong = function(score) {
-
-  for (var index = 1; index <= score; index +=1) {
-
-    if (index % 3 === 0 && index % 5 === 0) {
-      $("#list").append("<li>pingpong</li>");
-    } else if (index % 5 === 0){
-      $("#list").append("<li>pong</li>");
-    } else if (index % 3 === 0){
-      $("#list").append("<li>ping</li>");
-    } else {
-      $("#list").append("<li>" + index + "</li>");
-    }
-  };
-};
